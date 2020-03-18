24c023a1c54eb72ecf1daa3e347f976f0812231a
rodrigoge <rgestevao@gmail.com>
2020-03-18 18:49:11 -0300

    Testing creating user function and controller implementation

--- src/main/java/br/com/library/controllers/UserController.java
+++ src/main/java/br/com/library/controllers/UserController.java
@@ -1,11 +1,68 @@
 package br.com.library.controllers;
 
+import java.io.IOException;
 import java.io.Serializable;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import br.com.library.business.UserBusiness;
+import br.com.library.business.exception.BusinessException;
+import br.com.library.models.TypeUser;
+import br.com.library.models.User;
+import br.com.library.repositories.UserRepository;
+import br.com.library.utils.DataConfiguration;
+
+@ManagedBean
+@RequestScoped
 public class UserController implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	private User user = new User();
 	
+	public void init() {
+		if(this.user == null) {
+			this.user = new User();
+		}
+	}
+	
+	public void save() throws IOException{
+		EntityManager data = DataConfiguration.getEntityManager();
+		EntityTransaction transaction = data.getTransaction();
+		FacesContext context = FacesContext.getCurrentInstance();
+		
+		try {
+			transaction.begin();
+			UserBusiness userBusiness = new UserBusiness(new UserRepository(data));
+			userBusiness.save(user);
+			this.user = new User();
+			context.addMessage(null, new FacesMessage("Salvo com sucesso!"));
+			transaction.commit();
+		} catch (BusinessException e) {
+			transaction.rollback();
+			FacesMessage message = new FacesMessage(e.getMessage());
+			message.setSeverity(FacesMessage.SEVERITY_ERROR);
+			context.addMessage(null, message);
+		}finally {
+			data.close();
+		}
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	public TypeUser[] gettypeUser() {
+		return TypeUser.values();
+	}
 
 }
