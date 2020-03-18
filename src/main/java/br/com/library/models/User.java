2ba69f17b86d2e5d1f67e4b01f05321b22e5959f
rodrigoge <rgestevao@gmail.com>
2020-03-18 12:55:13 -0300

    Creating user model

---
+++ src/main/java/br/com/library/models/User.java
@@ -0,0 +1,89 @@
+package br.com.library.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String fullname;
+	private String username;
+	private String password;
+	private String address;
+	private String phone;
+	private String email;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFull_name() {
+		return fullname;
+	}
+	public void setFull_name(String full_name) {
+		this.fullname = full_name;
+	}
+	public String getUser_name() {
+		return username;
+	}
+	public void setUser_name(String user_name) {
+		this.username = user_name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
