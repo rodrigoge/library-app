e98571a8837c8aac15b3330b5b9d221c9255277c
rodrigoge <rgestevao@gmail.com>
2020-03-19 14:29:11 -0300

    Fixing errors of login and creating logout

--- src/main/java/br/com/library/controllers/LoginController.java
+++ src/main/java/br/com/library/controllers/LoginController.java
@@ -1,6 +1,7 @@
 package br.com.library.controllers;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.application.FacesMessage;
@@ -19,37 +20,43 @@ import br.com.library.utils.DataConfiguration;
 public class LoginController implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private User user = new User();
 	EntityManager data = DataConfiguration.getEntityManager();
 	private UserRepository userRepository = new UserRepository(data);
-	private List<User> allUsers;
-	
-	
+	private List<User> allUsers = new ArrayList<User>();
+	private String username = "";
+	private String password = "";
+
 	public void init() {
-		if(this.user == null) {
+		if (this.user == null) {
 			this.user = new User();
 		}
 	}
-	
+
 	public String login() {
 
-		allUsers = userRepository.all();
-		
-		if(allUsers.isEmpty()) {
-			FacesContext.getCurrentInstance().addMessage(null, 
-					new FacesMessage(FacesMessage.SEVERITY_ERROR, 
-							"Nome de usuário ou senha incorretos.", "Erro"));
+		allUsers = userRepository.searchLogin(user.getUsername(), user.getPassword());
+
+		if (allUsers.isEmpty()) {
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Nome de usuário ou senha incorretos.", "Erro"));
 			return null;
 		} else {
-			HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
-			
-			if(session != null) {
+			HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext()
+					.getSession(false);
+
+			if (session != null) {
 				session.setAttribute("user", allUsers);
 			}
-			
-			return "Home.xhtml";
+
+			return "/Home.faces?faces-redirect=true";
 		}
+	}
+
+	public String logout() {
+		FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+		return "/Login?faces-redirect=true";
 	}
 
 	public User getUser() {
@@ -71,5 +78,21 @@ public class LoginController implements Serializable {
 	public List<User> getAllUsers() {
 		return allUsers;
 	}
-	
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 }
--- src/main/java/br/com/library/repositories/UserRepository.java
+++ src/main/java/br/com/library/repositories/UserRepository.java
@@ -12,39 +12,45 @@ import br.com.library.utils.DataConfiguration;
 public class UserRepository implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private EntityManager data = DataConfiguration.getEntityManager();
-	
-	public UserRepository (EntityManager data) {
+
+	public UserRepository(EntityManager data) {
 		this.data = data;
 	}
-	
-	//search for a user
+
+	// search for a user
 	public User index(Long id) {
 		return data.find(User.class, id);
 	}
-	
-	//search for all users
-	public List<User> all(){
+
+	//search for users from login
+	public List<User> searchLogin(String username, String password) {
+		TypedQuery<User> query = data
+				.createQuery("select u from User u where u.username = :username and u.password = :password", User.class)
+				.setParameter("username", username).setParameter("password", password);
+		return query.getResultList();
+	}
+
+	// search for all users
+	public List<User> all() {
 		TypedQuery<User> query = data.createQuery("from User u order by u.id", User.class);
 		return query.getResultList();
 	}
-	
-	//add user 
+
+	// add user
 	public void create(User user) {
 		this.data.persist(user);
 	}
-	
-	//updates the user if it already exists
+
+	// updates the user if it already exists
 	public void update(User user) {
 		this.data.merge(user);
 	}
-	
-	//remove user
+
+	// remove user
 	public void delete(User user) {
 		this.data.remove(user);
 	}
-	
-	
 
 }
